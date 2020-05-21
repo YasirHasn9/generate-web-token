dfb00c2ff5bdb8d9c36803d345a3a075f08ad4c9
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-21 09:58:50 -0700

    user-router , find users in find fun

--- api/server.js
+++ api/server.js
@@ -4,10 +4,13 @@ const cors = require("cors");
 
 const server = express();
 
+const usersRouter = require("../users/users-router");
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
 server.get("/", (req, res) => {
   res.json({
     message: " Up "
